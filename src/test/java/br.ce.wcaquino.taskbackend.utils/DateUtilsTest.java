446bbd08819bf01cb989f4cb5ee7062e17238b6d
guilherme.campos <guilherme.silva.c@hotmail.com>
2021-06-23 17:33:19 -0400

    criado testes

---
+++ src/test/java/br.ce.wcaquino.taskbackend.utils/DateUtilsTest.java
@@ -0,0 +1,30 @@
+package br.ce.wcaquino.taskbackend.utils;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.time.LocalDate;
+
+public class DateUtilsTest {
+
+    @Test
+    public void deveRetornarTruePAraDatasFuturas() {
+        LocalDate date = LocalDate.of(2030, 01, 01);
+
+        Assert.assertTrue(DateUtils.isEqualOrFutureDate(date));
+    }
+
+    @Test
+    public void deveRetornarFalsoPAraDatasPAssadas() {
+        LocalDate date = LocalDate.of(2010, 01, 01);
+
+        Assert.assertFalse(DateUtils.isEqualOrFutureDate(date));
+    }
+
+    @Test
+    public void deveRetornarTruePAraDataAtual() {
+        LocalDate date = LocalDate.now();
+
+        Assert.assertTrue(DateUtils.isEqualOrFutureDate(date));
+    }
+}
